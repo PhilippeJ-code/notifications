90601aadd05444615a0b4ff705c395499b2ad4e9
PhilippeJ-code <jacobs.philippe@skynet.be>
2024-04-21 10:03:19 +0200

    Jeedom v4.4

--- desktop/php/notifications.php
+++ desktop/php/notifications.php
@@ -123,6 +123,18 @@
       <div role="tabpanel" class="tab-pane" id="commandtab">
         <a class="btn btn-success btn-sm cmdAction pull-right" data-action="add" style="margin-top:5px;"><i class="fa fa-plus-circle"></i> {{Commandes}}</a><br/><br/>
         <table id="table_cmd" class="table table-bordered table-condensed">
+          <thead>
+            <tr>
+              <th>{{Id}}</th>
+              <th>{{Nom}}</th>
+              <th>{{Type}}</th>
+              <th>{{Paramètres}}</th>
+              <th>{{Etat}}</th>
+              <th>{{Action}}</th>
+            </tr>
+          </thead>
+          <tbody>
+          </tbody>
         </table>
       </div>
     </div>
